0426588b56aff26414770dd5364322af95a00df6
shehan82 <shehansandeepa82@gmail.com>
2021-04-08 01:28:25 +0530

    signIn page done

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Database());
+        home: SignIn());
   }
 }
--- lib/signIn.dart
+++ lib/signIn.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -6,6 +7,26 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  AuthMethods authMethods = new AuthMethods();
+  TextEditingController emailTEC = new TextEditingController();
+  TextEditingController passwordTEC = new TextEditingController();
+
+  bool isLoading = false;
+
+  final formKey = GlobalKey<FormState>();
+
+  signMeIn() {
+    if (formKey.currentState.validate()) {
+      setState(() {
+        isLoading = true;
+      });
+
+      authMethods
+          .signInWithEmailAndPassword(emailTEC.text, passwordTEC.text)
+          .then((value) => print(value));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,30 +44,51 @@ class _SignInState extends State<SignIn> {
             // mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                padding: EdgeInsets.all(15),
-                child: TextField(
-                  // controller: userNameTEC,
-                  decoration: InputDecoration(
-                      hintText: "User Name",
-                      hintStyle: TextStyle(color: Colors.white, fontSize: 17),
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white))),
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(15),
-                child: TextField(
-                  // controller: emailTEC,
-                  decoration: InputDecoration(
-                      hintText: "Email",
-                      hintStyle: TextStyle(color: Colors.white, fontSize: 17),
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white))),
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
+              Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        child: TextFormField(
+                          validator: (val) {
+                            return RegExp(
+                                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                    .hasMatch(val)
+                                ? null
+                                : "please enter the valid email";
+                          },
+                          controller: emailTEC,
+                          decoration: InputDecoration(
+                              hintText: "Email",
+                              hintStyle:
+                                  TextStyle(color: Colors.white, fontSize: 17),
+                              enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white))),
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        child: TextFormField(
+                          validator: (val) {
+                            return val.length > 6
+                                ? null
+                                : "Please provide passowrd greater than 6";
+                          },
+                          obscureText: true,
+                          controller: passwordTEC,
+                          decoration: InputDecoration(
+                              hintText: "Password",
+                              hintStyle:
+                                  TextStyle(color: Colors.white, fontSize: 17),
+                              enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white))),
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  )),
               SizedBox(
                 height: 15,
               ),
@@ -66,16 +108,21 @@ class _SignInState extends State<SignIn> {
                 width: double.infinity,
                 margin: EdgeInsets.all(15),
                 height: 50,
-                child: Container(
-                  width: double.infinity,
-                  alignment: Alignment.center,
-                  child: Text(
-                    "Sign In",
-                    style: TextStyle(color: Colors.white, fontSize: 18),
+                child: InkWell(
+                  onTap: () {
+                    signMeIn();
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Sign In",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.indigo[700],
+                        borderRadius: BorderRadius.circular(25)),
                   ),
-                  decoration: BoxDecoration(
-                      color: Colors.indigo[700],
-                      borderRadius: BorderRadius.circular(25)),
                 ),
               ),
               Container(
