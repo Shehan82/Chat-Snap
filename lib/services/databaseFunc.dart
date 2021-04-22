c46f06d2f901c12ef66c27eea556f39c590314df
shehan82 <shehansandeepa82@gmail.com>
2021-04-22 15:54:23 +0530

    get field name form firestore

--- lib/services/databaseFunc.dart
+++ lib/services/databaseFunc.dart
@@ -1,12 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseFunctions {
-  getUserByUserName(userName) {
-    FirebaseFirestore.instance
+  getUserByUserName(userName) async {
+    return await FirebaseFirestore.instance
         .collection("users")
         .where("name", isEqualTo: userName)
         .get()
-        .then((value) => print(value));
+        .then((value) => print(value.docs[0].data()["name"]));
 
     // print(x);
   }
