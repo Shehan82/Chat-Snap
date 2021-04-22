3ccce2aa8ef8e61d8833506ffbe4fedf45572192
shehan82 <shehansandeepa82@gmail.com>
2021-04-22 13:31:26 +0530

    change uploadUser function

--- lib/services/databaseFunc.dart
+++ lib/services/databaseFunc.dart
@@ -2,6 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseFunctions {
   uploadUserInfo(userMap) {
-    FirebaseFirestore.instance.collection("users").add(userMap);
+    print(userMap);
+    print("helloooo");
+    FirebaseFirestore.instance.collection("users").add(userMap).catchError((e) {
+      print(e);
+    });
   }
 }
--- lib/signUp.dart
+++ lib/signUp.dart
@@ -1,6 +1,6 @@
 import 'package:chat_app/services/auth.dart';
 import 'package:chat_app/services/databaseFunc.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'signIn.dart';
